f1b8cb460626f2e5a51cae2997d027a0a44b2902
johngachara <johngachara29@gmail.com>
2025-11-15 10:04:10 +0300

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,24 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+    server: {
+        host: "::",
+        port: 8080,
+    },
     base: '/',
     build: {
         assetsInlineLimit: 0,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
     },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 }));

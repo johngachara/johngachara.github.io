4140f79759eb5071eb9d8b042de46db90344957b
johngachara <johngachara29@gmail.com>
2025-11-15 09:52:22 +0300

    debug github pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-    base: '/johngachara.github.io/',
+    base: '/',
     build: {
         assetsInlineLimit: 0,
     },
